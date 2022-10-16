2bcd913df221eb01b75f56ac18f7140a8cca48fb
Vamsi <kvksteja1@gmail.com>
2022-10-16 22:08:07 +0530

    model summary included in stage1 logs

--- src/01_base_model.py
+++ src/01_base_model.py
@@ -7,6 +7,7 @@ from src.utils.common import read_yaml, create_directories
 import random
 import tensorflow as tf
 import numpy as np
+import io
 
 
 STAGE = "base model creation"
@@ -52,7 +53,16 @@ def main(config_path):
     METRICS = ["accuracy"]
     model.compile(optimizer=OPTIMIZER, loss=LOSS,metrics=METRICS)
 
-    model.summary()
+    # log our model summary information in logs
+
+    def _log_model_summary(model):
+        with io.StringIO() as stream:
+            model.summary(print_fn= lambda x: stream.write(f"{x}\n"))
+            summary_str = stream.getvalue()
+        return summary_str
+
+    # model.summary()
+    logging.info(f"bin_scratch_model summary: \n{_log_model_summary(model)}")
 
     # Train the model
     history = model.fit(
